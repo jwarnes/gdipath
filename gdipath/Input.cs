e5efbc2125145d43fa233809c7576177df4e675e
Justin <jwarnes@gmail.com>
2012-11-20 11:54:54 -0500

    whoops, forgot a few files

---
+++ gdipath/Input.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gdipath
+{
+   public class Input
+    {
+
+        #region fields
+        protected bool accept;
+        #endregion
+
+        #region Keys API
+        public bool Accept { get { return accept; } }
+        #endregion
+
+        private List<bool> keyArray;
+
+        public Input()
+        {
+            keyArray = new List<bool>();
+            keyArray.Add(accept);
+        }
+        
+        public void KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+        
+        public void KeyUp(object sender, KeyEventArgs e)
+        {
+        }
+        
+        public void KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                accept = true;
+            }
+        }
+
+        public void ClearInput()
+        {
+            accept = false;
+        }
+    }
+}
---
+++ gdipath/MenuState.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace gdipath
+{
+    class MenuState : GameState
+    {
+        #region Fields
+
+        #endregion
+
+        public MenuState(GameStateManager manager): base (manager)
+        {
+            this.manager = manager;
+        }
+
+        public override void Load()
+        {
+            
+        }
+
+        public override void Input()
+        {
+            Input input = manager.Game.Input;
+            if (input.Accept)
+            {
+                Kill();
+            }
+        }
+
+
+        public override void Process()
+        {
+           
+        }
+
+        public override void Draw(Graphics g)
+        {  
+            Image face = Image.FromFile(@"img\butt.gif");
+            g.DrawString("Pathfinding Example", new Font("Verdana", 16, FontStyle.Bold), Brushes.White,
+                 30, 100);
+
+            g.DrawString("PUSH ENTER TO GO TO THE NEXT THING", new Font("Verdana", 14, FontStyle.Regular), Brushes.White,
+                 74, 200);
+
+            g.DrawImage(face, 50, 50);
+        }
+
+    }
+}
